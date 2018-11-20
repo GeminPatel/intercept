1644980553981e40c750bdc4afbf4b5a8e09a5a8
Gemin Patel <gemin.patel@payu.in>
2018-11-20 19:16:27 +0530

    changes Builder class to Worker class

--- lib/intercept.rb
+++ lib/intercept.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'intercept/version'
-require 'intercept/builder'
+require 'intercept/worker'
 
 module Intercept
 
--- lib/intercept/worker.rb
+++ lib/intercept/worker.rb
@@ -5,9 +5,14 @@ require 'intercept/strategy/mapper'
 require 'intercept/strategy/white_list'
 
 module Intercept
-  class Builder
+  # Base class of the intercept module.
+  # A worker object is responsible to intercept and modify state of registering class's objects
+  class Worker
     attr_reader :interceptor_map, :decorator_map
 
+    # @param registration_method [String, Symbol] method_name which is called by the registering class.
+    # @param interceptor_map [Hash] Keys represent fields to be modified. Values are Strategy class object
+    # @param decorator_map [Hash] Keys represent fields to be modified. Values are Decorator class object
     def initialize(registration_method, interceptor_map = {}, decorator_map = {})
       @interceptor_map = interceptor_map
       @decorator_map = decorator_map
@@ -17,6 +22,9 @@ module Intercept
       end
     end
 
+    # This method is called when we want to intercept and modify an object.
+    # 'registration_method' parameter of initialize is an alias to this method.
+    # @param message is an object of any registering class
     def intercept(message)
       interceptor_map.each do |field, strategy|
         if message.respond_to?(field) && message.respond_to?("#{field}=")
