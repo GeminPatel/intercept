dbda585278b7a5e95ff38b244a0fced9008dca5b
Gemin Patel <gemin.patel@payu.in>
2018-11-20 18:21:13 +0530

    changing method name to make it more general

--- lib/intercept/builder.rb
+++ lib/intercept/builder.rb
@@ -10,12 +10,12 @@ module Intercept
       @interceptor_map = interceptor_map
       @decorator_map = decorator_map
 
-      if registration_method != 'delivering_message' && registration_method != :delivering_message
+      if registration_method != 'intercept' && registration_method != :intercept
         define_singleton_method(registration_method) { |message| delivering_message(message) }
       end
     end
 
-    def delivering_message(message)
+    def intercept(message)
       interceptor_map.each do |field, strategy|
         if message.respond_to?(field) && message.respond_to?("#{field}=")
           message.public_send("#{field}=", strategy.process_identities(message.public_send field))
